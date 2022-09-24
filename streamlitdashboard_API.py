055c194053e947870bff98b7e89f3e777b2f8bf3
liliben80 <linda.benali@yahoo.fr>
2022-09-25 00:52:29 +0200

    test

--- streamlitdashboard_API.py
+++ streamlitdashboard_API.py
@@ -238,7 +238,7 @@ def main():
         if uploaded_file is not None:
             headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
             feature = pd.read_csv(uploaded_file,delimiter=";")
-            feature = json.dumps(feature)
+            feature = json.dumps(feature.to_dict())
             st.write(feature)
             url = "https://myappwithgithub.herokuapp.com/predict_model"
             response = requests.post(url, data=feature, headers=headers)
