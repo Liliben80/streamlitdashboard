5303d67e9ee55548acffae86622ef83653855f28
liliben80 <linda.benali@yahoo.fr>
2022-09-25 10:42:37 +0200

    swt

--- streamlitdashboard_API.py
+++ streamlitdashboard_API.py
@@ -238,7 +238,7 @@ def main():
         if uploaded_file is not None:
             headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
             st.write(uploaded_file)
-            feature = open(uploaded_file,"w")
+            feature = open(uploaded_file.name,"w")
             feature = feature.read()
             feature = json.dumps(feature)
 
