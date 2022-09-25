95d0b5f535e4c116f9e795951009093f43882a7d
liliben80 <linda.benali@yahoo.fr>
2022-09-26 01:14:15 +0200

    test

--- streamlitdashboard_API.py
+++ streamlitdashboard_API.py
@@ -250,86 +250,8 @@ def main():
             st.write(feature)
             url = "https://myappwithgithub.herokuapp.com/predict_model"
             response = requests.post(url, data=feature, headers=headers)
-
-            try:
-                proba = response.json()['Probability']
-                classe = response.json()['Classe']
-            except json.JSONDecodeError as identifier:
-                print("Error occur", identifier.msg)
-
-            # --- MODEL APPLICATION
-            st.markdown(f'*Probabilité de solvabilité: {proba}*')
-            st.markdown(f'*Crédit accepté: {classe}*')
-
-#             feature = pd.read_csv(uploaded_file, delimiter=";")
-#             # --- MODEL APPLICATION
-#             probability = model.predict_proba(feature)
-#             # I dont'understand
-#             probability = str(probability)
-#             probability = probability.split()[0]
-#             probability = probability.replace("[[", "")
-#             probability = float(probability)
-#             decision = "OUI" if probability > proba_threshold else "NON"
-#             st.markdown(f'*Probabilité de solvabilité: {probability}*')
-#             st.markdown(f'*Crédit accepté: {decision}*')
-
-            ### --- PROBABILITY GAUGE
-            plot_bgcolor = "#def"
-            quadrant_colors = [plot_bgcolor, "#2bad4e", "#f25829"]
-            quadrant_text = ["", "<b>Accepté</b>", "<b>Refusé</b>"]
-            n_quadrants = len(quadrant_colors) - 1
-
-            #current_value = probability
-            current_value = proba
-            min_value = 0
-            max_value = 1
-            hand_length = np.sqrt(2) / 4
-            hand_angle = np.pi * (1 - (max(min_value, min(max_value, current_value)) - min_value) / (max_value - min_value))
-
-            fig = go.Figure(
-                data=[
-                    go.Pie(
-                        values=[0.5] + (np.ones(n_quadrants) / 2 / n_quadrants).tolist(),
-                        rotation=90,
-                        hole=0.5,
-                        marker_colors=quadrant_colors,
-                        text=quadrant_text,
-                        textinfo="text",
-                        hoverinfo="skip",
-                    ),
-                ],
-                layout=go.Layout(
-                    showlegend=False,
-                    margin=dict(b=0,t=10,l=10,r=10),
-                    width=450,
-                    height=450,
-                    paper_bgcolor=plot_bgcolor,
-                    annotations=[
-                        go.layout.Annotation(
-                            text=f"<b>Solvabilité:</b><br>{current_value}",
-                            x=0.5, xanchor="center", xref="paper",
-                            y=0.25, yanchor="bottom", yref="paper",
-                            showarrow=False,
-                        )
-                    ],
-                    shapes=[
-                        go.layout.Shape(
-                            type="circle",
-                            x0=0.48, x1=0.52,
-                            y0=0.48, y1=0.52,
-                            fillcolor="#333",
-                            line_color="#333",
-                        ),
-                        go.layout.Shape(
-                            type="line",
-                            x0=0.5, x1=0.5 + hand_length * np.cos(hand_angle),
-                            y0=0.5, y1=0.5 + hand_length * np.sin(hand_angle),
-                            line=dict(color="#333", width=4)
-                        )
-                    ]
-                )
-            )
-            st.plotly_chart(fig)
+            st.write(response)
+              st.plotly_chart(fig)
 
 # @st.cache(allow_output_mutation=True)
 @st.cache
