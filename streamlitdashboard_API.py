d713d3420a4f8f1302029800a557dcbcd3a3002f
liliben80 <linda.benali@yahoo.fr>
2022-09-25 11:20:31 +0200

    swt

--- streamlitdashboard_API.py
+++ streamlitdashboard_API.py
@@ -240,7 +240,7 @@ def main():
             #feature = open(uploaded_file,"w")
             #feature = feature.read()
             #feature = pd.read_csv(uploaded_file)
-            feature = np.load('feature.npy', allow_pickle='TRUE')
+            feature = np.load(uploaded_file, allow_pickle='TRUE')
             feature = json.dumps(feature)
 
             url = "https://myappwithgithub.herokuapp.com/predict_model"
