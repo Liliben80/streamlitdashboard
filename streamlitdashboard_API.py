1d86ebb693e95b3d37f083f71ea9e500fa4d8682
liliben80 <linda.benali@yahoo.fr>
2022-09-26 07:00:17 +0200

    test

--- streamlitdashboard_API.py
+++ streamlitdashboard_API.py
@@ -242,7 +242,7 @@ def main():
     else:
         st.title("Prédiction de nouveau client")
         #uploaded_file = st.file_uploader("Importer les caractéristiques du client (fichier CSV).")
-        input_vector = st.text_area("Entrer les caractéristiques du client.",st.session_state.var[key])
+        input_vector = st.text_area("Entrer les caractéristiques du client.", **kwargs)
         ### --- SELECTION OF VECTOR
         if input_vector is not None:
             headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
